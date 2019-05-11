b38a9e9be0a324937d4a65ffcf3e5945b8dceda4
Alexandra Briggs <alex.m.briggs@gmail.com>
2019-05-11 17:36:57 -0400

    created a playlist model

---
+++ src/app/models/playlist.model.ts
@@ -0,0 +1,13 @@
+export class Playlist {
+    id: number;
+    name: string;
+    account: Account;
+    tracks: list;
+
+    constructor(id:number, name:string, account:Account, tracks:list) {
+        this.id,
+        this.name,
+        this.account,
+        this.tracks
+    }
+}

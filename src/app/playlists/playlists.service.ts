c023cb774044bfecb864512cfb98fe3d48af0c7e
Alexandra Briggs <alex.m.briggs@gmail.com>
2019-05-11 20:53:05 -0400

    added error handler to get request in playlist service

--- src/app/playlists/playlists.service.ts
+++ src/app/playlists/playlists.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs/Ob";
+import { Observable } from "rxjs/Observable";
 import 'rxjs/Rx';
 
 import {Playlists} from './playlists'
@@ -12,6 +12,12 @@ export class PlaylistsService{
     constructor(private http: HttpClient) {}
 
     searchPlaylists(name:string): Observable<Playlists[]> {
-        return this.http.get<Playlists[]>("http://localhost:8088/playlists/${name});
+        return this.http
+            .get<Playlists[]>("http://localhost:8088/playlists/${name}")
+            .catch(this.handleError);
+    }
+
+    private handleError(error: Response){
+        return Observable.throw(error.statusText);
     }
 }
